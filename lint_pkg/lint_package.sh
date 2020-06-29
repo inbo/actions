4c79e42669038fb6b7f7e080775c224d05795e8f
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2020-06-29 22:18:28 +0200

    handle errors during lintr::lint_package()

--- lint_pkg/lint_package.sh
+++ lint_pkg/lint_package.sh
@@ -8,10 +8,14 @@ cd $3
 echo '\nInstalling the package...\n'
 Rscript -e 'remotes::install_local(quiet = TRUE)'
 echo '\nChecking linters in package...\n'
-Rscript --no-save --no-restore -e 'lintr::lint_package()' > ../output.Rout
+Rscript --no-save --no-restore -e 'lintr::lint_package()' 1> ../output.Rout 2> ../error.Rout
 if [ -s ../output.Rout ] ; then
   cat ../output.Rout;
   echo '\nPlease check the coding style\n';
+  exit 1
+elif [ -s ../error.Rout ] ; then
+  cat ../error.Rout;
+  echo '\nCheck failed. Please check the error message above.\n';
   exit 1
 else
   echo '\nYour code is lint free. Good job!\n';
